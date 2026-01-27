0ef220908adcf58c738a33cdacf7c7fa3df3b5ed
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-27 19:46:07 +0100

    Update CVPreview.tsx

--- components/CVPreview.tsx
+++ components/CVPreview.tsx
@@ -1,3 +1,9 @@
+<button
+  onClick={() => window.print()}
+  className="mb-4 px-4 py-2 bg-black text-white rounded-lg print:hidden"
+>
+  Last ned PDF
+</button>
 import { CV } from "@/types/cv"
 
 export default function CVPreview({ cv }: { cv: CV }) {
