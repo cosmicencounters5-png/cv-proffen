28bad2f15787e3f3bb1881eb75d580b6934b88c3
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-28 10:08:14 +0100

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
-
 export const supabaseServer = createClient(
-  supabaseUrl,
-  supabaseServiceRoleKey
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
