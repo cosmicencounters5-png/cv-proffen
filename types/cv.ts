bd9eb58eea125b3aed8bf7af5559e43b5ce46bad
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-28 13:39:10 +0100

    Update cv.ts

--- types/cv.ts
+++ types/cv.ts
@@ -1,30 +1,38 @@
+export type PersonalInfo = {
+  firstName: string
+  lastName: string
+  title: string
+  email: string
+  phone: string
+}
+
+export type Experience = {
+  id: string
+  role: string
+  company: string
+  from: string
+  to: string
+  description?: string
+}
+
+export type Education = {
+  id: string
+  school: string
+  degree: string
+  from: string
+  to: string
+}
+
+export type Skill = {
+  id: string
+  name: string
+}
+
 export type CV = {
   id: string
-  personal: {
-    firstName: string
-    lastName: string
-    title: string
-    email: string
-    phone: string
-  }
+  personal: PersonalInfo
   summary?: string
-  experience: {
-    id: string
-    role: string
-    company: string
-    from: string
-    to: string
-    description?: string
-  }[]
-  education: {
-    id: string
-    school: string
-    degree: string
-    from: string
-    to: string
-  }[]
-  skills: {
-    id: string
-    name: string
-  }[]
+  experience: Experience[]
+  education: Education[]
+  skills: Skill[]
 }
