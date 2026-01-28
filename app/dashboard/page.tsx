6cce7c10a13f32cb19a2b18554476e344358b9e1
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-28 11:01:34 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { supabaseClient } from "@/lib/supabaseClient"
 
 export default function DashboardPage() {
   const router = useRouter();
