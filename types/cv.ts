f6cbdab6bbe65d91af206fa3c65d5df082cd0d1d
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-28 09:19:23 +0100

    Update cv.ts

--- types/cv.ts
+++ types/cv.ts
@@ -1,30 +1,26 @@
 export type CV = {
   id: string
-
   personal: {
     firstName: string
     lastName: string
-    title: string // 👈 Profesjonell tittel (viktig)
+    title: string
     email: string
     phone: string
   }
-
   experience: {
     id: string
     role: string
     company: string
-    from: string
-    to: string
+    from?: string
+    to?: string
     description?: string
   }[]
-
   education: {
     id: string
-    degree: string
     school: string
-    year: string
+    degree: string
+    year?: string
   }[]
-
   skills: {
     id: string
     name: string
