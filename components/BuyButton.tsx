ffced33b3a5394513965b060bcea4d8bd164c163
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-01-28 19:11:39 +0100

    Update BuyButton.tsx

--- components/BuyButton.tsx
+++ components/BuyButton.tsx
@@ -1,9 +1,17 @@
 "use client"
 
-export default function BuyButton() {
+type Props = {
+  packageType: "cv_only" | "cv_and_application"
+}
+
+export default function BuyButton({ packageType }: Props) {
   const handleBuy = async () => {
     const res = await fetch("/api/stripe/checkout", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ packageType }),
     })
 
     const data = await res.json()
@@ -20,7 +28,7 @@ export default function BuyButton() {
       onClick={handleBuy}
       className="bg-black text-white px-6 py-3 rounded text-sm"
     >
-      Kjøp CV (test)
+      Kjøp CV
     </button>
   )
 }
