fd7c57e1154ccade0e12d35260ed665f13f98bc4
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-03 01:46:52 +0100

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,28 +1,33 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const urls = [
-    { loc: "https://cv-proffen.no/", priority: "1.0", changefreq: "weekly" },
-    { loc: "https://cv-proffen.no/lage-cv", priority: "0.9", changefreq: "monthly" },
-    { loc: "https://cv-proffen.no/jobbsoknad", priority: "0.9", changefreq: "monthly" },
-    { loc: "https://cv-proffen.no/cv-offentlig-sektor", priority: "0.8", changefreq: "monthly" },
-  ];
-
+export function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls
-  .map(
-    (u) => `
   <url>
-    <loc>${u.loc}</loc>
-    <changefreq>${u.changefreq}</changefreq>
-    <priority>${u.priority}</priority>
-  </url>`
-  )
-  .join("")}
+    <loc>https://cv-proffen.no/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://cv-proffen.no/lage-cv</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://cv-proffen.no/jobbsoknad</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://cv-proffen.no/cv-offentlig-sektor</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
 </urlset>`;
 
   return new NextResponse(xml, {
-    headers: { "Content-Type": "application/xml; charset=utf-8" },
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
   });
 }
