578beeb4fe9d6a47a87d1e2e3c67545f691a00d6
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-03 01:23:16 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://cv-proffen.no/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://cv-proffen.no/lage-cv",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://cv-proffen.no/jobbsoknad",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://cv-proffen.no/cv-offentlig-sektor",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
