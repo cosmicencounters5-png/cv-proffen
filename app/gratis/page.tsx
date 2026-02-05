6d1a29a9b3ad0861e71f29f6f27ba5953b7bfed2
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-05 08:23:34 +0100

    Update page.tsx

--- app/gratis/page.tsx
+++ app/gratis/page.tsx
@@ -4,15 +4,22 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function GratisPage() {
+
   const router = useRouter();
 
   useEffect(() => {
+
     // SET FREE TRIAL FLAG
     localStorage.setItem("cvproffen_free_trial", "true");
 
     // SEND USER TO REGISTER
     router.push("/register");
-  }, []);
 
-  return null;
+  }, [router]);
+
+  return (
+    <main style={{ padding: "4rem" }}>
+      Aktiverer gratis tilgang...
+    </main>
+  );
 }
